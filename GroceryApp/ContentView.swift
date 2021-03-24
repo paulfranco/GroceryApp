25a6c9984c24ace5828e81a2ddd689da1c18de84
Paul Franco <mail@paulfran.co>
2021-03-24 09:58:37 -0700

    Initial Commit

---
+++ GroceryApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GroceryApp
+//
+//  Created by Paul Franco on 3/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Paul Franco on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
